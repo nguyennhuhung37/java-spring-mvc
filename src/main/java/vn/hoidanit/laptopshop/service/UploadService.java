cd63e15475b0bf04e45439cadf84d61d4dafc588
NGUYEN NHU HUNG <nguyennhuhungdh@gmail.com>
2024-04-16 09:55:16 +0700

    #91

---
+++ src/main/java/vn/hoidanit/laptopshop/service/UploadService.java
@@ -0,0 +1,43 @@
+package vn.hoidanit.laptopshop.service;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import jakarta.servlet.ServletContext;
+import java.io.FileOutputStream;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadService {
+
+    private ServletContext servletContext;
+
+    public UploadService(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+    public String handleSaveUploadFile(MultipartFile file, String targetFolder) {
+        String rootPath = this.servletContext.getRealPath("/resources/images");
+        String finalName = "";
+        try {
+            byte[] bytes;
+            bytes = file.getBytes();
+
+            File dir = new File(rootPath + File.separator + targetFolder);
+            if (!dir.exists())
+                dir.mkdirs();
+            // Create the file on server
+            finalName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
+            File serverFile = new File(dir.getAbsolutePath() + File.separator + finalName);
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return finalName;
+    }
+}
