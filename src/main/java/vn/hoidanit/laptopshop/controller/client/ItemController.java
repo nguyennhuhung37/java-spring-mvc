d20ecad24c99ceacade0a33d190579ea690e702e
NGUYEN NHU HUNG <nguyennhuhungdh@gmail.com>
2024-04-25 21:03:07 +0700

    #98

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -0,0 +1,15 @@
+package vn.hoidanit.laptopshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class ItemController {
+    @GetMapping("/product/{id}")
+    public String getProductDetailPage(Model model, @PathVariable long id) {
+        return "client/product/detail";
+    }
+}
